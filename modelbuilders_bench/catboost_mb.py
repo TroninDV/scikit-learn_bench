4e79a00afd8a429fb9d4c4bdb4c82aebb2d28560
TroninDV <tronindmitr@gmail.com>
2021-08-17 14:34:41 +0300

    Add threads

--- modelbuilders_bench/catboost_mb.py
+++ modelbuilders_bench/catboost_mb.py
@@ -74,12 +74,12 @@ parser.add_argument('--scale-pos-weight', type=float, default=1,
 parser.add_argument('--subsample', type=float, default=1,
                     help='Subsample ratio of the training instances')
 
-parser.add_argument('--test-size', type=int, default=-1)
+parser.add_argument('--threads', type=int, default=100,
+                    help='Number of threads')
 
 params = bench.parse_args(parser)
 
-if params.test_size == -1:
-    params.test_size = None
+daal4py.daalinit(params.threads)
 
 X_train, X_test, y_train, y_test = bench.load_data(params)
 
